b76669a47af2659e2052ead9e5c88dc1c2247b5c
Ivonete Tecla Gomes <lucasvicenti60@gmail.com>
2022-07-20 12:59:36 -0300

    commit inicial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import NoteList from "./components/NoteList";
+import { nanoid } from 'nanoid';
+import Search from "./components/Search";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  const [notes, setNotes] = useState([]);
+
+  const addNote = (text) => {
+    const date = new Date();
+    const newNote = {
+      id: nanoid(),
+      text: text,
+      date: date.toLocaleDateString()
+    }
+    const newNotes = [...notes, newNote];
+    setNotes(newNotes);
+  };
+
+  const deleteNote = (id) => {
+    const newNotes = notes.filter((note) => note.id !== id);
+    setNotes(newNotes);
+  }
+
+  return (<div className="container">
+    <Search/>
+    <NoteList notes={notes} handleAddNote={addNote} handleDeleteNote={deleteNote}/>
+  </div>);
 }
-
 export default App;
